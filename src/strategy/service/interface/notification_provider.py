224e7e91c398b075449c99d4c03e5695520e6a34
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2025-05-15 22:35:35 -0300

    refactor: Rename abstract notification class
    - Change class name to NotificationProvideer instead of NotificationServiceStrategy
    - Change file name to notification_provider instead of notification_strategy

--- src/strategy/service/interface/notification_provider.py
+++ src/strategy/service/interface/notification_provider.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 
-class NotificationStrategy(ABC):
+class NotificationProvider(ABC):
     """This interface will define the abstract behavior of a notification strategy""" 
-    
+
     @abstractmethod
     def notify(self, message: str) -> None:
         """This method will notify the user with the given message"""
